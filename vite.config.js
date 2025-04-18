67cb9b3bb9affc2e9a1fc4f600556a150b9e1ba7
Diego <mora8mola@gmail.com>
2025-04-18 13:23:14 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/EjerciciosCSS3/",
   build: {
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
   },
 });
